a29c4a6798fd0edb4c3a2d3b2bca25827d5d2ed8
lukasnordangard <lukasnordangard@fastmail.fm>
2023-12-18 14:19:33 +0100

    Added code to Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import View.RadioInfoUI;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        RadioInfoUI ui = new RadioInfoUI();
 
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ui.createAndShowGUI();
+        });
     }
 }
