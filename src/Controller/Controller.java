9404b1da99e23ea4d734ca0ec88d139dc7c0d8e3
lukasnordangard <lukasnordangard@fastmail.fm>
2023-12-26 18:51:34 +0100

    Updated Program information

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -133,15 +133,27 @@ public class Controller {
 
             for (int i = 0; i < scheduleNodes.getLength(); i++) {
                 Element scheduleElement = (Element) scheduleNodes.item(i);
+
+                // Retrieve information from XML
+                int episodeId = Integer.parseInt(scheduleElement.getElementsByTagName("episodeid").item(0).getTextContent());
                 String name = scheduleElement.getElementsByTagName("title").item(0).getTextContent();
+                String description = scheduleElement.getElementsByTagName("description").item(0).getTextContent();
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
                 LocalDateTime startTime = LocalDateTime.parse(scheduleElement.getElementsByTagName("starttimeutc").item(0).getTextContent(), formatter);
                 LocalDateTime endTime = LocalDateTime.parse(scheduleElement.getElementsByTagName("endtimeutc").item(0).getTextContent(), formatter);
+                int programId = Integer.parseInt(scheduleElement.getElementsByTagName("program").item(0).getAttributes().getNamedItem("id").getTextContent());
+                String imageUrl = scheduleElement.getElementsByTagName("imageurl").item(0).getTextContent();
 
                 // Filter programs based on time
                 if (filterProgram(startTime, endTime)) {
-                    Program program = new Program(name, startTime, endTime);
+                    Program program = new Program(episodeId, programId, name, description, startTime, endTime, imageUrl);
                     programs.add(program);
+                    System.out.println("episodeId: " + episodeId);
+                    System.out.println("name: " + name);
+                    System.out.println("description: " + description);
+                    System.out.println("programId: " + programId);
+                    System.out.println("imageUrl: " + imageUrl);
+                    System.out.println("===================");
                 }
             }
         } catch (Exception e) {
--- src/Model/Program.java
+++ src/Model/Program.java
@@ -3,18 +3,34 @@ package Model;
 import java.time.LocalDateTime;
 
 public class Program {
+    private final int episodeId;
+    private final int programId;
     private final String name;
+    private final String description;
     private final LocalDateTime startTime;
     private final LocalDateTime endTime;
+    private final String imageUrl;
 
-    public Program(String name, LocalDateTime startTime, LocalDateTime endTime) {
+    public Program(int episodeId, int programId, String name, String description, LocalDateTime startTime, LocalDateTime endTime, String imageUrl) {
+        this.episodeId = episodeId;
+        this.programId = programId;
         this.name = name;
+        this.description = description;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.imageUrl = imageUrl;
     }
+
+    public int getEpisodeId() { return episodeId; }
+
+    public int getProgramId() { return programId; }
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public LocalDateTime getStartTime() {
